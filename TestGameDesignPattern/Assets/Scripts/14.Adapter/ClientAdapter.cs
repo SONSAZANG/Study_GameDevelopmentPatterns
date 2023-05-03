5504ab74998ec945a220a9394d0fc5110ec422da
Andrew_손은수 <esson@maxst.com>
2023-05-03 12:57:20 +0900

    add 15.Adapter

---
+++ TestGameDesignPattern/Assets/Scripts/14.Adapter/ClientAdapter.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chapter.Adapter
+{
+    public class ClientAdapter : MonoBehaviour
+    {
+        public InventoryItem item;
+        private InventorySystem inventorySystem;
+        private IInventorySystem inventorySystemAdapter;
+
+        private void Start()
+        {
+            inventorySystem = new InventorySystem();
+            inventorySystemAdapter = new InventorySystemAdapter();
+        }
+
+        private void OnGUI()
+        {
+            if (GUILayout.Button("Add item (no adapter)"))
+                inventorySystem.AddItem(item);
+
+            if (GUILayout.Button("Add item (with adapter)"))
+                inventorySystemAdapter.AddItem(item, SaveLocation.Both);
+        }
+    }
+}
---
+++ TestGameDesignPattern/Assets/Scripts/14.Adapter/InventorySystemAdapter.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chapter.Adapter
+{
+    public class InventorySystemAdapter : InventorySystem, IInventorySystem
+    {
+        private List<InventoryItem> cloudInventory;
+
+        public void SyncInventories()
+        {
+            var cloudInventory = GetInventory();
+
+            Debug.Log("Synchronizing local drive and cloud inventories");
+        }
+
+        public void AddItem(InventoryItem item, SaveLocation location)
+        {
+            if (location == SaveLocation.Cloud)
+                AddItem(item);
+
+            if (location == SaveLocation.Local)
+                Debug.Log("Adding item to local drive");
+
+            if (location == SaveLocation.Both)
+                Debug.Log("Adding item to local drive and on the cloud");
+        }
+
+        public void RemoveItem(InventoryItem item, SaveLocation location)
+        {
+            Debug.Log("Remove item from local/cloud/both");
+        }
+
+        public List<InventoryItem> GetInventory(SaveLocation location)
+        {
+            Debug.Log("Get inventory from local/cloud/both");
+
+            return new List<InventoryItem>();
+        }
+
+    }
+}
